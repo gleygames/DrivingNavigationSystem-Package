477b8cba478d736a820dadd4a7107ed6da65eb1a
Gley <gleygames@hotmail.com>
2026-09-18 20:51:23 +0300

    Step 1 done

--- Editor/AssemblyInfo.cs
+++ Editor/AssemblyInfo.cs
@@ -1,6 +1,13 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Gley.NavigationSystem.Tests.Editor")]
+[assembly: InternalsVisibleTo("Gley.NavigationSystem.Dev.Editor")]
+[assembly: InternalsVisibleTo("Gley.NavigationSystem.Editor.URP")]
+[assembly: InternalsVisibleTo("Gley.NavigationSystem.Editor.HDRP")]
+
 namespace Gley.NavigationSystem.Editor
 {
-    public class AssemblyInfo 
+    public class AssemblyInfo
     {
     }
 }
--- Runtime/AssemblyInfo.cs
+++ Runtime/AssemblyInfo.cs
@@ -1,6 +1,17 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Gley.NavigationSystem.Editor")]
+[assembly: InternalsVisibleTo("Gley.NavigationSystem.Tests.Editor")]
+[assembly: InternalsVisibleTo("Gley.NavigationSystem.Tests.Runtime")]
+[assembly: InternalsVisibleTo("Gley.NavigationSystem.Tests.Shared")]
+[assembly: InternalsVisibleTo("Gley.NavigationSystem.TMP")]
+[assembly: InternalsVisibleTo("Gley.NavigationSystem.InputSystem")]
+[assembly: InternalsVisibleTo("Gley.NavigationSystem.Dev")]
+[assembly: InternalsVisibleTo("Gley.NavigationSystem.Dev.Editor")]
+
 namespace Gley.NavigationSystem
 {
-    public class AssemblyInfo 
+    public class AssemblyInfo
     {
     }
 }
